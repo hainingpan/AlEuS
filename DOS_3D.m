9b87bf9a5a4e6357e146c4f86e74b86dd43ef2d4
Haining Pan <jack_pan1994@126.com>
2021-02-16 18:34:36 -0500

    Create DOS_3D.m

---
+++ DOS_3D.m
@@ -0,0 +1,15 @@
+function [Elist,dos]=DOS_3D(energyall,param)
+energyall_sort=sort(vertcat(energyall{:}));
+
+eta=1e-4;
+Emin=energyall_sort(1);
+Emax=energyall_sort(end);
+Elist=linspace(Emin,Emax,1000);
+deltaf=1/pi*eta./((Elist-energyall_sort).^2+eta^2); %axis 1: state; axis 2: Elist
+dos=sum(deltaf,1);
+
+dos=dos/size(energyall,1)/(prod(param.a/5.076e-3));  % of eV^-1 nm^-1
+figure;plot(Elist,dos);
+xlabel('E (meV)');
+ylabel('DOS(eV^{-1}nm^{-3})');
+end
