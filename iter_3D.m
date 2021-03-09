31a7238b848eb3a9e9bffbba6ad7f980fecb6c83
Haining Pan <jack_pan1994@126.com>
2021-03-09 16:41:47 -0500

    remove parallel optiosn
    1.remove parallel options
    2. add "verbose" option

--- iter_3D.m
+++ iter_3D.m
@@ -1,4 +1,4 @@
-param=main_3D('g',10,'N',[60,100,50],'ED',50*433*8.617333262e-5,'pbc',0,'Ez',0);
+param=main_3D('g',10,'N',[60,100,50],'ED',50*433*8.617333262e-5,'pbc',0,'Ez',0,'verbose',0);
 
 kindex=1:param.N(3);
 uz=(2*kindex-param.N(3)-1)/(2*param.N(3));
