0b8080237482b5ddc8cabe4f15a12ee54d0c0574
Haining Pan <jack_pan1994@126.com>
2021-03-22 16:10:49 -0400

    correct mistakes of energy at kz for Al and FM
    1. change the kreq from 50 to 20
    2. add energy at kz for Al and FM respectively

--- epsilon_3D_bilayer.m
+++ epsilon_3D_bilayer.m
@@ -1,3 +1,5 @@
-function re=epsilon_3D_bilayer(k,param)
-re=2*param.t_Al(3)*(1-cos(param.a(3)*k));
+function [energy_Al,energy_FM]=epsilon_3D_bilayer(k,param)
+energy_Al=2*param.t_Al(3)*(1-cos(param.a(3)*k));
+energy_FM=2*param.t_FM(3)*(1-cos(param.a(3)*k));
+
 end
--- iter_3D_bilayer.m
+++ iter_3D_bilayer.m
@@ -1,11 +1,12 @@
-param=main_3D_bilayer('N_Al',[60,100,50],'N_FM',[20,100,50],'g',5.55,'ED',50*433*8.617333262e-5);
+param=main_3D_bilayer('N_Al',[60,100,50],'N_FM',[1,100,50],'g',5.55,'ED',50*433*8.617333262e-5);
 
 kindex=1:param.N(3);
 uz=(2*kindex-param.N(3)-1)/(2*param.N(3));
 param.klist=uz*param.b(3); 
-param.energylist=epsilon_3D_bilayer(param.klist,param);
+[param.energylist_Al,param.energylist_FM]=epsilon_3D_bilayer(param.klist,param);
 
-ave0=3.5e-4/param.g*[zeros(param.N_FM(1)*param.N_FM(2),1);ones(param.N_Al(1)*param.N_Al(2),1)];
+ave0=.0001814/param.g*[zeros(param.N_FM(1)*param.N_FM(2),1);ones(param.N_Al(1)*param.N_Al(2),1)];
+% ave0=3.5e-4/param.g*[zeros(param.N_FM(1)*param.N_FM(2),1);ones(param.N_Al(1)*param.N_Al(2),1)];
 
 [energyall,wfall]=energyMF_3D_bilayer(ave0,cell(param.N(3),1),param);
  
